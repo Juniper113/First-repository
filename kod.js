e0d3b3f1f72f2ce2ca6c3754b61a8d84840ba667
Juniper113 <61781110+Juniper113@users.noreply.github.com>
2020-03-07 14:40:10 +0100

    Update kod.js

--- kod.js
+++ kod.js
@@ -3,10 +3,11 @@ circumference: 0,
 area: 0,
 };
 
-const calculateArea = function (circle, r) {
-circle.circumference = 2 * MathPI * r;
+const calculateArea = function(circle, r) {
+circle.circumference = 2 * Math.PI * r;
 circle.area = Math.PI * r * r;
 return circle;
 };
 
 console.log(calculateArea(myCircle, 5));
+
