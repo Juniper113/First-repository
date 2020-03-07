9365fc7189797b33268b5f78366f992534dd2e2c
Juniper113 <61781110+Juniper113@users.noreply.github.com>
2020-03-07 13:36:32 +0100

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,9 +1,10 @@
 let calculateArea = function(r) {
   let circumference = 2 * Math.PI * r;
   let area = Math.PI * r * r;
+  return result
   console.log(circumference);
   console.log(area);
 };
 
-calculateArea(5);
+calculateArea();
 
